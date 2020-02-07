81670123440cabe30d66b31bbf4276160fe3fa53
jackie-4321 <jackielok1223@gmail.com>
2020-02-07 11:50:29 -0600

    second.commit

--- store.js
+++ store.js
@@ -1,5 +1,6 @@
 const shopItemBtn = document.querySelectorAll('.shop-item-button');
 const cartItems = document.querySelector('.cart-items')
+
 //get input
 shopItemBtn.forEach(function(elem) {
     elem.addEventListener('click', (e) => {
@@ -8,27 +9,78 @@ shopItemBtn.forEach(function(elem) {
         let image = elem.parentElement.previousElementSibling.src;
         let name = elem.parentElement.previousElementSibling.previousElementSibling.innerHTML;
         let quantity = 1;
-        addToCart(image, name, price, quantity);
+        let totalPrice = document.querySelector('.cart-total-price');
+        let totalPriceNum = parseFloat(totalPrice.innerHTML);
+        let allItems = document.getElementsByClassName('cart-item-title');
+        let arrayItems = Array.from(allItems);
+        addToCart(image, name, price, quantity);       
     })
 })
 
 //add to cart
 function addToCart(image,name,price, quantity) {
-    cartItems.innerHTML +=`
-    <div class="cart-row">
-        <div class="cart-item cart-column">
-            <img src="${image}" width="100" height="100">
-            <span class="cart-item-title">${name}</span>
-        </div>
-        <div class="cart-price cart-column">${price}</div>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="${quantity}">
-            <button class="btn btn-danger" type="button" >REMOVE</button>
-        </div>
+    // let allItems = document.getElementsByClassName('cart-item-title');
+    // let arrayItems = Array.from(allItems);
+    let totalPrice = document.querySelector('.cart-total-price');
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    let totalPriceNum = parseFloat(totalPrice.innerHTML)
+    if (cartItemNames.length == 0) {
+        cartItems.innerHTML +=`
+            <div class="cart-row">
+            <div class="cart-item cart-column">
+                <img src="${image}" width="100" height="100">
+                <span class="cart-item-title">${name}</span>
+            </div>
+            <div class="cart-price cart-column">${price}</div>
+            <div class="cart-quantity cart-column">
+                <input class="cart-quantity-input" type="number" value="${quantity}">
+                <button class="btn btn-danger" type="button" >REMOVE</button>
+            </div>
+        
+            </div>
+        `;
+        total(price, quantity);
+    } else {
+        // arrayItems.forEach(function(item) {
+        //     console.log(item)
+        console.log(cartItemNames)
+        for (var i = 0; i < cartItemNames.length; i++) {
+            if (cartItemNames[i].innerText == name) {
+                alert('The item is in the cart already')
+                return
+            }
+        }
+         
+                cartItems.innerHTML +=`
+                <div class="cart-row">
+                <div class="cart-item cart-column">
+                    <img src="${image}" width="100" height="100">
+                    <span class="cart-item-title">${name}</span>
+                </div>
+                <div class="cart-price cart-column">${price}</div>
+                <div class="cart-quantity cart-column">
+                    <input class="cart-quantity-input" type="number" value="${quantity}">
+                    <button class="btn btn-danger" type="button" >REMOVE</button>
+                </div>
+            
+                </div>
+            `;
+            total(price, quantity);
+    }
+
+                
+
+
     
-    </div>
-    `;
-    total(price, quantity)
+}
+    
+
+
+
+
+
+
 
     //delete button
     let removeBtn = document.getElementsByClassName('btn btn-danger');
@@ -45,61 +97,46 @@ function addToCart(image,name,price, quantity) {
 
         })
     })
-}
+
 
 //update total of multiple items 
 function total(price, quantity) {
     //convert price from string to number
     let totalPrice = document.querySelector('.cart-total-price');
+    let totalPriceNum = parseFloat(totalPrice.innerHTML);
     let newPrice = parseFloat(price.substr(1));
 
     //add the new total price
-        let totalPriceNum = parseFloat(totalPrice.innerHTML);
         if (!totalPriceNum) {
             let sum = newPrice * quantity;
             totalPrice.innerHTML = sum.toFixed(2);
-
-                //get the updated quantity and total price 
-                let newQuantity = document.querySelectorAll('.cart-quantity-input');
-                newQuantity.forEach(function(item) {
-                let totalPrice = document.querySelector('.cart-total-price');
-                let totalPriceNum = parseFloat(totalPrice.innerHTML);
- 
-                item.addEventListener('focus', (e) => {
-                        let oldTotal = item.value * newPrice;
-                        item.addEventListener('change', (e) => {
-                            let finalTotal = totalPriceNum - oldTotal + (item.value * newPrice);
-                            item.defaultValue = item.value;
-                            totalPrice.innerHTML = finalTotal.toFixed(2);
-                            })
-                        }) 
-                    })
+            updateCartTotal()
             totalPrice.innerHTML = sum.toFixed(2);
         } else {
             totalPriceNum += newPrice * quantity;
-               //get the updated quantity and total price 
-               let newQuantity = document.querySelectorAll('.cart-quantity-input');
-               newQuantity.forEach(function(item) {
-               let totalPrice = document.querySelector('.cart-total-price');
-               let totalPriceNum = parseFloat(totalPrice.innerHTML);
-
-               item.addEventListener('focus', (e) => {
-                newPrice = parseFloat(item.parentElement.previousElementSibling.innerHTML.substr(1));
-                        let oldTotal = item.value * newPrice;
-                       totalPriceNum = parseFloat
-                       (totalPrice.innerHTML) 
-                       item.addEventListener('change', (e) => {
-                           let finalTotal = totalPriceNum - oldTotal + (item.value * newPrice);
-                           item.defaultValue = item.value;
-                           totalPrice.innerHTML = finalTotal.toFixed(2);
-                           })
-                       }) 
-                   })
-            
-
+            updateCartTotal()
             totalPrice.innerHTML = totalPriceNum.toFixed(2);
-        }
-            
+        }  
 }
 
- 
+function updateCartTotal() {
+    //get the updated quantity and total price 
+    let newQuantity = document.querySelectorAll('.cart-quantity-input');
+    newQuantity.forEach(function(item) {
+        let totalPrice = document.querySelector('.cart-total-price');
+        let totalPriceNum = parseFloat(totalPrice.innerHTML);
+        
+        //get the old total before changing the quantity
+        item.addEventListener('focus', (e) => {
+            newPrice = parseFloat(item.parentElement.previousElementSibling.innerHTML.substr(1));
+            let oldTotal = item.value * newPrice;
+            totalPriceNum = parseFloat(totalPrice.innerHTML);
+            //update the new total price after chaning the quantity
+            item.addEventListener('input', (e) => {
+                let finalTotal = totalPriceNum - oldTotal + (item.value * newPrice);
+                item.defaultValue = item.value;
+                totalPrice.innerHTML = finalTotal.toFixed(2);
+                })
+        }) 
+    })
+}
